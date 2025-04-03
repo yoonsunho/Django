c9679daf0e83bddd6ce0d90e32aec0a340a97eee
yoonsunho <y_sunho@naver.com>
2025-04-03 17:11:03 +0900

    edit

--- 08-02-authentication-system/accounts/forms.py
+++ 08-02-authentication-system/accounts/forms.py
@@ -1,6 +1,6 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model  # 현재 프로젝트에서 활성화된 사용자 모댈반환
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import User
+# from .models import User
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
