6eb38af223dca45373dc9aa10051c94aa6f7fa58
yoonsunho <y_sunho@naver.com>
2025-05-11 17:51:03 +0900

    edit 09-02drf

--- 09-01-DRF/articles/views.py
+++ 09-01-DRF/articles/views.py
@@ -8,15 +8,18 @@ from .serializers import ArticleListSerializer, ArticleSerializer
 # Create your views here.
 @api_view(['GET','POST'])
 def article_list(request):
+
     if request.method =='GET':
         # 전체 게시글 데이터 조회
         articles = Article.objects.all()
         # articles는 django에서는 쓸 수 있는 queryset 데이터 타입이기 때문에, 
         # 우리가 만든 모델 시리얼라이저로 변환 진행
-        serializer = ArticleListSerializer(articles, many= True)
+        # many = True : serialize 대상이 queryset인 경우 입력
+        serializer = ArticleListSerializer(articles, many= True)        
         # DRF에서 제공하는 Response를 사용해 JSON데이터를 응답
         # JSON 데이터는 serializer의  DATA속성에 존재
         return Response(serializer.data)
+        # Serialized data 객체에서 실제 데이터 추출
     
     # 게시글 생성 요청에 대한 응답
     elif request.method == 'POST':
@@ -24,10 +27,12 @@ def article_list(request):
         # form = ArticleForm(request.POST)
         # 사용자 입력 데이터를 클래스로 받아서 변환
         serializer = ArticleSerializer(data = request.data)
+
         # 유효성 검사
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status = status.HTTP_201_CREATED)
+                                               
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
         
 
@@ -44,8 +49,11 @@ def article_detail(request, article_pk):
         return Response(status= status.HTTP_204_NO_CONTENT)
     elif request.method =='PUT':
         # 사용자가 보낸 수정 데이터를 변환
+        # serializer = ArticleSerializer(instance = article,data = request.data, partial= True)
         serializer = ArticleSerializer(article, data = request.data, partial = True)    # 부분수정 가능
         if serializer.is_valid(raise_exception = True):
+
+            
             serializer.save()
             return Response(serializer.data)
         # return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
